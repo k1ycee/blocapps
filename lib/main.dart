54428dd829b6e562dda86ac033450e2fca171647
Denzel <chiagozieani145@gmail.com>
2019-12-04 22:27:34 +0100

    This is a counter app with no business logic in it's UI which is truly what want.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:blocapps/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,52 +12,61 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BlocProvider<CounterBloc>(
+        create: (context) => CounterBloc(),
+        child: CounterPage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class CounterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Counter'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (context, count){
+          return Center(
+            child: Text(
+              '$count',
+              style: TextStyle(
+                fontSize: 24,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: (){
+                counterBloc.add(CounterEvent.decrement);
+              }
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+          ),
+          SizedBox(height: 5.0),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: (){
+                  counterBloc.add(CounterEvent.increment);
+                }
+            ),
+          ),
+        ],
+      )
     );
   }
 }
+
